Fix normalization, un-reverse cost argsort, DCEM indexing

MPC.py
@@ -353,9 +353,8 @@ class MPC:
 
             if self.ac_buf.shape[0] > 0:
                 action, self.ac_buf = self.ac_buf[0], self.ac_buf[1:]
-                if d_random:
-                    return action
-                return self.possible_actions[np.argmax(action)]
+                print("picked act: ", action)
+                return action
             self.sy_cur_obs = obs
 
             # (Resolved) What is soln here?
@@ -371,14 +370,18 @@ class MPC:
                 # (Resolved) I don't understand what this concat is doing.
                 # After we are done with the current action, we pop it
                 # and append a new entry.
-                # self.prev_sol = np.concatenate([np.copy(soln)[1:], np.zeros((1, self.per * self.dU))])
-                self.prev_sol = np.concatenate([np.copy(soln)[1:], np.zeros((1, self.per * self.env.action_space.n))])
+                # self.prev_sol: shape (plan_hor, self.dU)
+                # soln: shape (plan_hor, self.env.action_space.n):  probabilities over all discrete actions
+                # all_remaining_actions: shape ((plan_hor - self.per), env.action_space.n): actions we discard bc we only take the first `self.per` actions
+                all_remaining_actions = np.copy(soln)[self.per:] 
+                # self.prev_sol: shape (plan_hor, env.action_space.n), add rest of acs in horizon as zeros
+                self.prev_sol = np.concatenate([all_remaining_actions, np.zeros((self.per, self.env.action_space.n))], axis=0)
 
-                # (Resolved) original code does not convert prob back to action
-                # Fill buffer with the __action__ by selecting the action
-                # with max probability.
-                # self.ac_buf = soln[:1].reshape(-1, self.dU)
-                self.ac_buf = self.env.possible_actions[np.argmax(soln[:1])]
+                # only put self.per actions into the buffer
+                # next_acs: (self.per, self.dU)
+                next_acs = np.take(self.env.possible_actions, np.argmax(soln[:self.per], axis=-1), axis=0)
+                # self.ac_buf: (self.per, self.dU)
+                self.ac_buf = next_acs
 
             return self.act(obs, t)
 
@@ -393,10 +396,14 @@ class MPC:
             self.sy_cur_obs = obs
 
             # `optimizer.obtain_solution` calls this MPC's `compile_cost`, which computes the cost of the current `self.sy_cur_obs` (loool)
+            # soln: (plan_hor,)
             soln = self.optimizer.obtain_solution(self.prev_sol, self.init_var)
             # we reoptimize the ac seq every `self.per` timesteps, so get the next batch of acs
+            # prev_soln: (plan_hor,)
             self.prev_sol = np.concatenate([np.copy(soln)[self.per * self.dU:], np.zeros(self.per * self.dU)])
+            # ac_buf: (self.per, ac_dim)
             self.ac_buf = soln[:self.per * self.dU].reshape(-1, self.dU)
+            import pdb; pdb.set_trace()
 
             return self.act(obs, t)
 
@@ -435,6 +442,8 @@ class MPC:
             # on the boundary and reset std to 0.
 
 #            print("max next obs: ", torch.max(next_obs).cpu().numpy(), " // mean ", torch.mean(next_obs).cpu().numpy())
+            print("cur_obs: ", cur_obs)
+            print("acs: ", cur_acs)
             cost = self.obs_cost_fn(next_obs) + self.ac_cost_fn(cur_acs)
 
             # One difference between CARL and this work is that
@@ -465,6 +474,7 @@ class MPC:
         else:
             # cost shape: (popsize, npart)
             mean_cost = costs.mean(dim=1)
+        print("mean cost: ", mean_cost.mean())
         return mean_cost.detach().cpu().numpy()
 
     # FIXME: predicts non-sensical states

config/PointmassEasy-v0.py
@@ -86,7 +86,7 @@ class PointmassEasyConfigModule:
         """Constructs target for training model.
 
         Returns: (note we do NOT predict pendulum len)
-            (state delta, catastrophe_prob) 
+            ((normalized) state delta, catastrophe_prob) 
         """
         state_delta = next_obs[..., :2] - obs[..., :2]
         next_catastrophe_prob = next_obs[...,-1:]
@@ -100,8 +100,7 @@ class PointmassEasyConfigModule:
         """
         # TODO: this is dense reward specifically
         print("next obs: ", obs[:, :2])
-        print("goal: ", obs[:, 2:4])
-        return -torch.norm(obs[:, :2] - obs[:, 2:4], dim=-1)
+        return torch.norm(obs[:, :2] - obs[:, 2:4], dim=-1)
 
     @staticmethod
     def ac_cost_fn(acs):

env/pointmass.py
@@ -375,10 +375,9 @@ class PointmassEnv(gym.Env):
 
       if len(self.obs_vec) != 0:
         self.replay_buffer = np.concatenate([self.replay_buffer, self.obs_vec.copy()], axis=0)
-      _, starting_rwd = self.get_dist_and_reward(self.fixed_start)
       self.obs_vec = [np.concatenate([
           self._normalize_obs(self.fixed_start.copy()),
-          self.fixed_goal,
+          self.goal,
           [0] # catastrophe
         ], axis=-1)]
       self.state = self.fixed_start.copy()
@@ -387,7 +386,7 @@ class PointmassEnv(gym.Env):
 
       return np.concatenate([
           self._normalize_obs(self.state.copy()),
-          self.fixed_goal,
+          self.goal,
           [0]], axis=-1)
 
   def reset_model(self, seed=None):
@@ -432,20 +431,6 @@ class PointmassEnv(gym.Env):
           state = new_state
     return state
     
-
-  def get_optimal_action(self, state):
-    state = self._unnormalize_obs(state)
-    best_action = 0
-    best_dist = np.inf
-    for i in range(self.num_actions):
-      action = np.array(ACT_DICT[i])
-      s_prime = self.simulate_step(state, action)
-      dist = self._get_distance(s_prime, self.fixed_goal)
-      if dist < best_dist:
-        best_dist = dist
-        best_action = i
-    return best_action
-
   def _discretize_state(self, state, resolution=1.0):
     (i, j) = np.floor(resolution * state).astype(np.int)
     # Round down to the nearest cell if at the boundary.
@@ -479,6 +464,10 @@ class PointmassEnv(gym.Env):
     return (self._walls[i, j] == 1)
 
   def get_dist_and_reward(self, state):
+    """
+    Args:
+        state: UNNORMALIZED state observation (in [0, 10] instead [0, 1]
+    """
     if (isinstance(state, torch.Tensor)):
         state = state.detach().cpu().numpy()
     # print('State: ' + str(state))
@@ -528,7 +517,7 @@ class PointmassEnv(gym.Env):
     # 2. resize_factor of the walls
     extended_obs = np.concatenate([
             normalized_obs,
-            self.fixed_goal,
+            self.goal,
             [int(catastrophe)]
         ], axis=-1)
 
@@ -614,7 +603,8 @@ class PointmassEnv(gym.Env):
 
     # Annotate rewards
     for i in range(len(obs_vec)):
-        self.plt.annotate(f"{obs_vec[i, 2]:.1f}", obs_vec[i, :2])
+        unnormalized_obs = self._unnormalize_obs(obs_vec[i, :2])
+        self.plt.annotate(f"{obs_vec[i, 2]:.1f}", self.get_dist_and_reward(unnormalized_obs))
 
     # Draw a rewarded states for sparse rewards
     # Draw catastrophe states
@@ -624,7 +614,8 @@ class PointmassEnv(gym.Env):
         if e[-1] != 0:
             catastrophic_states.append(e)
         if not self.dense_reward:
-            d, re = self.get_dist_and_reward(e[:2])
+            unnormalized_obs = self._unnormalize_obs(e[:2])
+            d, re = self.get_dist_and_reward(unnormalized_obs)
             if re >= 0:
                 rewarding_states.append(e)
     

optimizers.py
@@ -175,10 +175,19 @@ class DiscreteCEMOptimizer(Optimizer):
     # instead of action id, because the action will be fed into 
     # _predict_next_obs
     def sample_from_categorical(self, probs, possible_actions, num_samples):
+        """
+        Args:
+            probs (prev_sol): shape (self.plan_hor, num_poss_acs), ac probabilities for each step of plan_hor
+            possible_actions: list of possible actions, length `num_poss_acs`
+            num_samples: number of samples to draw
+        Returns:
+            Draws `num_samples`, each sample with `self.plan_hor` actions drawn from the prob dist of acs for that timestep
+            samples: shape (num_samples, self.plan_hor, ac_dim)
+        """
         probs += 0.001 # prevent torch from thinking anything is < 0
+        # shape (num_samples, self.plan_hor)
         samples = torch.distributions.categorical.Categorical(probs=torch.from_numpy(probs)).sample([num_samples])
-        
-        samples = possible_actions[samples.numpy()].astype(np.float32)
+        samples = np.take(possible_actions, samples.numpy(), axis=0).astype(np.float32)
         return samples
 
     # performing one hot encoding
@@ -186,20 +195,21 @@ class DiscreteCEMOptimizer(Optimizer):
         """Optimizes the cost function using the provided initial candidate distribution
 
         Arguments:
-            init_mean (np.ndarray): The mean of the initial candidate distribution.
+            init_mean (np.ndarray): The mean of the initial candidate distribution. shape (plan_hor, ac_dim)
             possible_actions (np.ndarray): The possible actions this discrete env allows
+        Returns:
+            mean action probability (across elite samples) for the horizon
+                (plan_hor, ac_dim)
         """
         mean, t = init_mean, 0
         while t < self.max_iters:
 
-            # Should this return a series of concrete actions
+            # samples: shape (popsize, plan_hor, ac_dim)
             samples = self.sample_from_categorical(mean, possible_actions, self.popsize)
-            # Shape here should be (400, 25, 2)
-            # samples = samples.reshape(self.popsize, self.sol_dim)
-            samples = samples.reshape(self.popsize, mean.shape[0], int(self.sol_dim / mean.shape[0]))
+            assert samples.shape == (self.popsize, mean.shape[0], possible_actions[0].shape[-1])
 
             costs = self.cost_function(samples)
-            elites = samples[np.argsort(-costs)][:self.num_elites]
+            elites = samples[np.argsort(costs)][:self.num_elites]
 
             # Cast actions to probabilities
             # Elites are concrete action samples. [0, -1],