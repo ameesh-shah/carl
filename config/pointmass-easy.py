416ca6b7b6be26d3c5dac3e177cff952bcc31ab9
Jessy Lin <jessy81697@gmail.com>
2020-12-14 17:17:07 -0800

    Refactor pointmass config

---
+++ config/pointmass-easy.py
@@ -0,0 +1,6 @@
+from config.pointmass import PointmassConfigModule
+
+class PointmassEasyConfigModule(PointmassConfigModule):
+    ENV_NAME = "PointmassEasy-v0"
+
+CONFIG_MODULE = PointmassEasyConfigModule
